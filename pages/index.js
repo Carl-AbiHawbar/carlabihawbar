9c40f9b8670c40be1c88f2f0ea6701a6c626eedc
Carl-AbiHawbar <carlabihawbar434@gmail.com>
2023-04-19 00:40:15 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import PromProposal from '@/components/PromProposal'
+import PromProposal from '../components/PromProposal'
 
 const inter = Inter({ subsets: ['latin'] })
 
