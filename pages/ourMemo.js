4eda5e5ee110b14ef1613f416a3edd3660557cac
Carl-AbiHawbar <carlabihawbar434@gmail.com>
2023-04-26 00:23:15 +0300

    test

--- pages/ourMemo.js
+++ pages/ourMemo.js
@@ -1,10 +1,21 @@
-const ourMemo = () => {
-    return (
-      <video controls>
-        <source src="/video.mp4" type="video/mp4" />
+import React, { useRef } from 'react';
+
+function ourMemo(props) {
+  const videoRef = useRef(null);
+
+  function handlePlay() {
+    videoRef.current.play();
+  }
+
+  return (
+    <div>
+      <video ref={videoRef} width="640" height="360" controls>
+        <source src="public/myvideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-    )
+      <button onClick={handlePlay}>Play</button>
+    </div>
+  );
 }
 
-export default ourMemo
+export default ourMemo;
