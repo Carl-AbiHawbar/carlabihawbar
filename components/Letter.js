c6a14fc06a9992e85e45d8ae35756febbe24c7ad
Carl-AbiHawbar <108028259+Carl-AbiHawbar@users.noreply.github.com>
2023-12-19 22:20:11 +0200

    push

---
+++ components/Letter.js
@@ -0,0 +1,24 @@
+// components/Letter.js
+import React from 'react';
+
+const Letter = () => {
+  return (
+    <div className="letterContainer">
+      <div className="letterContent">
+        <h1>Dear Fos,</h1>
+        <p>
+          I wrote you this letter to tell you how much i love you and how much i enjoy making memories with you,
+          also i wanted to thank you for every small thing you took care of, i made this small gift and im telling you that one day
+          i will make you proud.
+        </p>
+        <p>
+          Btw i did not use chatgpt to write this letter HAHAHAHAHAHHAHAHA BHEBIK FOS MAY HART MY FAV MY WAYF MAY ALLLLLL.
+        </p>
+        <p>Sincerely,</p>
+        <p>Ur Huzby Carl.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Letter;
