f22d81253dafa58dc40202fb938d8d2a1ae7868b
Carl-AbiHawbar <108028259+Carl-AbiHawbar@users.noreply.github.com>
2023-12-27 10:27:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
-// pages/index.js
-import React from 'react';
+import React, { useState } from 'react';
 import Letter from '../components/Letter';
 
 const Home = () => {
-  return <Letter />;
+  const [password, setPassword] = useState('');
+  const [showLetter, setShowLetter] = useState(false);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Simple password for demonstration purposes
+    const correctPassword = 'password123';
+
+    if (password === correctPassword) {
+      setShowLetter(true);
+    } else {
+      alert('Incorrect password. Try again.');
+    }
+  };
+
+  return (
+    <div>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <button onClick={handleSubmit}>Submit</button>
+
+      {showLetter && <Letter />}
+    </div>
+  );
 };
 
 export default Home;
